a8882caf6a1939c90c60f883efbe11769729d4e5
Stephen Walsh <Stephenfionnwalsh@gmail.com>
2022-03-22 15:39:40 +0100

    start initial project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
