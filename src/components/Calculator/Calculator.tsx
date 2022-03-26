8693a77c334f78d6942522d029fab38ba02a5892
Stephen Walsh <Stephenfionnwalsh@gmail.com>
2022-03-26 14:47:12 +0100

    Calculations - work on calculation methods - display calculation accurately

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import Button from "../Button/Button";
 import CalculatorWrapper from "../CalculatorWrapper/CalculatorWrapper";
@@ -6,126 +6,193 @@ import Formula from "../Formula/Fomula";
 import Output from "../Output/Output";
 
 const Calculator = () => {
-  const [currentSelectedCharacter, setCurrentselectedCharacter] = useState("0");
+  const [answer, setAnswer] = useState("0");
   const [formula, setFormula] = useState("");
 
-  useEffect(() => {
-    if (currentSelectedCharacter !== "0") {
-      setFormula(formula + currentSelectedCharacter);
-    }
-  }, [currentSelectedCharacter]);
-
-  const getSelectedCharacterOnClick = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  const numberButtonClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    if (event.currentTarget.value !== "=")
-      setCurrentselectedCharacter(event.currentTarget.value);
+    // If there is a full completed equation then when we click on a number it starts a new equation
+    if (formula.match(/=/)) {
+      setFormula(e.currentTarget.value);
+      setAnswer(e.currentTarget.value);
+      return;
+    }
+
+    const currentNumber = e.currentTarget.value;
+
+    setFormula((prev) => prev + currentNumber);
+    if (answer === "0" || answer.match(/(-|\+|\/|x)/g)) {
+      setAnswer(currentNumber);
+    } else {
+      setAnswer((prev) => prev + currentNumber);
+    }
+  };
+
+  const operatorButtonClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const currentOperator = e.currentTarget.value;
+    const getLastChar = formula.charAt(formula.length - 1);
+    const checkCharForPattern = getLastChar.match(/(-|\+|\/|x)/g);
+
+    if (formula.match(/=/)) {
+      setFormula(answer);
+    }
+
+    // if there ar two operators and the last is a minus. We need to check this so we can perform arithmetic operastions on negative numbers
+    if (checkCharForPattern && currentOperator === "-") {
+      setAnswer(currentOperator);
+      setFormula((prev) => prev + currentOperator);
+    }
+
+    // The below condition needs to check if the previous char was an operator and to overwrite it unless it was a minus
+    if (checkCharForPattern && currentOperator !== "-") {
+      setAnswer(currentOperator);
+      const sliceFormula = formula.match(/(^\d+)/g);
+      setFormula(sliceFormula + currentOperator);
+    }
+
+    if (!checkCharForPattern) {
+      setAnswer(currentOperator);
+      setFormula((prev) => prev + currentOperator);
+    }
+  };
+
+  const addDecimal = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (answer.toString().includes(".")) return;
+    if (formula.includes("=")) {
+      setAnswer(".");
+      setFormula(".");
+    } else {
+      setAnswer((prev) => prev + ".");
+      setFormula((prev) => prev + ".");
+    }
+  };
+
+  const clear = () => {
+    setAnswer("0");
+    setFormula("");
+  };
+
+  const evaluate = () => {
+    if (!formula) return;
+    if (formula.match(/=/)) return;
+
+    // check if each sequence of numbers starts with multiple zeros and removes them
+    const splitFormula = formula
+      .split(/(-|\+|\/|x)/g)
+      .map((item) => item.replace(/^0+/g, ""))
+      .reduce((a, b) => a + b, "");
+    const result = eval(splitFormula.replace(/x/g, "*"));
+    setFormula((prev) => `${prev}=${result}`);
+    setAnswer(result);
   };
 
   return (
     <CalculatorWrapper>
       <Formula formula={formula} />
-      <Output output={currentSelectedCharacter} />
+      <Output output={answer} />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={clear}
         buttonType="horizontalRectangleRed"
         value="AC"
         id="clear"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={operatorButtonClick}
         buttonType="square"
         value="/"
         id="divide"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={operatorButtonClick}
         buttonType="square"
         value="x"
         id="multiply"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={numberButtonClick}
         buttonType="square"
         value="7"
         id="seven"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={numberButtonClick}
         buttonType="square"
         value="8"
         id="eight"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={numberButtonClick}
         buttonType="square"
         value="9"
         id="nine"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={operatorButtonClick}
         buttonType="square"
         value="-"
         id="minus"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={numberButtonClick}
         buttonType="square"
         value="4"
         id="four"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={numberButtonClick}
         buttonType="square"
         value="5"
         id="five"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={numberButtonClick}
         buttonType="square"
         value="6"
         id="six"
       />
       <Button
-        onClick={getSelectedCharacterOnClick}
+        onClick={operatorButtonClick}
         buttonType="square"
         value="+"
         id="add"
       />
       <div>
         <Button
-          onClick={getSelectedCharacterOnClick}
+          onClick={numberButtonClick}
           buttonType="square"
           value="1"
           id="one"
         />
         <Button
-          onClick={getSelectedCharacterOnClick}
+          onClick={numberButtonClick}
           buttonType="square"
           value="2"
           id="two"
         />
         <Button
-          onClick={getSelectedCharacterOnClick}
+          onClick={numberButtonClick}
           buttonType="square"
           value="3"
           id="three"
         />
         <div>
           <Button
-            onClick={getSelectedCharacterOnClick}
+            onClick={numberButtonClick}
             buttonType="horizontalRectangleGrey"
             value="0"
             id="zero"
           />
           <Button
-            onClick={getSelectedCharacterOnClick}
+            onClick={addDecimal}
             buttonType="square"
             value="."
             id="decimal"
           />
           <Button
-            onClick={getSelectedCharacterOnClick}
+            onClick={evaluate}
             buttonType="verticalRectangle"
             value="="
             id="equals"
